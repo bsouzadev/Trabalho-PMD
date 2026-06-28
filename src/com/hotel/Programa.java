4b8946333c4e521cf5216cf26ffde2bca770e031
joaoak1172 <muzzijoaov@gmail.com>
2026-06-28 16:35:59 -0300

    tirei o menu antigo da main e botei a janela principal

--- src/com/hotel/Programa.java
+++ src/com/hotel/Programa.java
@@ -1,10 +1,11 @@
 package src.com.hotel;
 
-import src.com.hotel.visao.Menu;
+import src.com.hotel.visao.JanelaPrincipal;
 
 public class Programa {
     public static void main(String[] args) {
-        Menu.MenuPrincipal();
+        JanelaPrincipal janela = new JanelaPrincipal();
+        janela.setVisible(true);
     }
 }
 
